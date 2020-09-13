818822874bf4d124c6d9de92b7734e3ca169ea32
Derek Worthen <worthend.derek@gmail.com>
2020-09-13 14:15:42 -0700

    refactor: global type names.

--- src/global.ts
+++ src/global.ts
@@ -1,8 +1,8 @@
 interface IAppHost {
   configPath: string;
   configure: (...appExtensions: AppHostExtension[]) => Record<string, unknown>;
-  get: <T>(key: string) => T | undefined;
-  set: <T>(key: string, obj: T) => IAppHost;
+  get: <T>(objectPath: string) => T | undefined;
+  set: <T>(objectPath: string, obj: T) => IAppHost;
   merge: (config: Record<string, unknown>, key?: string) => IAppHost;
 }
 
